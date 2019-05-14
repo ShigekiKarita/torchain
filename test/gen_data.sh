eb7e1367042177ce814af745f16519739369c6b3
ShigekiKarita <shigekikarita@gmail.com>
2019-05-14 17:55:09 +0900

    add test data

--- test/gen_data.sh
+++ test/gen_data.sh
@@ -19,15 +19,16 @@ outdir=test/res
 # lores_train_data_dir=data/${train_set}
 
 tmpdir=.torchain
+mkdir -p $outdir
 
 echo "generating MFCC subset"
-utils/subset_data_dir.sh --first data/train 10 $tmpdir/feat_hires
-apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$tmpdir/utt2spk scp:$tmpdir/cmvn.scp scp:$tmpdir/feats.scp ark:- | subset-feats --n 10 > $outdir/mfcc.ark
+featdir=$tmpdir/feat_hires
+utils/subset_data_dir.sh --first data/train 10 $featdir
+apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$featdir/utt2spk scp:$featdir/cmvn.scp scp:$featdir/feats.scp ark:- | subset-feats --n=10 ark:- ark:- > $outdir/mfcc.ark
 
 echo "generating supervision on subset"
 utils/subset_data_dir.sh --first data/train 10 $tmpdir/feat
-steps/align_fmllr_lats.sh --nj 1 --cmd "$train_cmd" $tmpdir/feat \
-                          data/lang exp/tri3b $tmpdir/lat
+steps/align_fmllr_lats.sh --nj 1 --cmd "$train_cmd" $tmpdir/feat data/lang exp/tri3b $tmpdir/lat
 
 lattice-align-phones --replace-output-symbols=true exp/chain_simple/tri3b_train_lats/final.mdl "ark:gunzip -c $tmpdir/lat/lat.1.gz |" ark:- | chain-get-supervision --lattice-input=true --frame-subsampling-factor=3 --right-tolerance=5 --left-tolerance=5 exp/chain_simple/tdnn1g/tree exp/chain_simple/tdnn1g/0.trans_mdl ark:- ark:- > $outdir/supevision.ark
 
